6f95e4f78638948b510de646bc4b0ceacbc1da25
Scott Warren <scott@Scotts-MacBook-Air-3.local>
2025-11-25 13:13:58 -0500

    some Player meta

--- character/Character.py
+++ character/Character.py
@@ -1,3 +1,21 @@
+from typing import List
+from item.Item import Item
+
+
 class Character:
-    def __init__(self):
-        pass
+    """Base class for the living characters within the game
+
+    Attributes:
+        name (str): name of the characters
+        inventory (List[Item]): list of items within characters inventory
+        hp (int): amount of health the character has
+        mana (int): amount of mana the character has
+    """
+
+    def __init__(self, name: str = "", inventory: List[Item] | None = None,
+                 hp: int = 0, mana: int = 0) -> None:
+        """Initializes the Base Character Class"""
+        self.name = name
+        self.inventory = inventory
+        self.hp = hp
+        self.mana = mana
