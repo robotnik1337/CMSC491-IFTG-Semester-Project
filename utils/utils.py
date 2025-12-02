07d4372324c44cab674ea89c4bc515be748bd487
Cnm4211 <coopermeekins03@gmail.com>
2025-12-02 16:21:01 -0500

    Implemented Character Classes, Item, Armor, Weapon, PickClass

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,10 @@
 def pick_class():
-    pass
+    print("What class would you like to be?")
+    choice = input("\n1. Warrior \n2. Mage \n")
+    while choice not in ["1", "2"]:
+        choice = input("Invalid choice. Please pick 1 or 2: \n")
+    return "Warrior" if choice == "1" else "Mage"
+
 
 def parser():
     pass
